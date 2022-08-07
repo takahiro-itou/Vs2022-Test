1211d1f57c01b56a48b48b36ec0a84c0fe501d84
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-08-07 18:34:14 +0900

    適当にフォームをデザイン

--- Sample/ViewVBNet6/Form1.vb
+++ Sample/ViewVBNet6/Form1.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub CountToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CountToolStripMenuItem.Click
+
+    End Sub
 End Class
