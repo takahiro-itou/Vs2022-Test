a9bc867eb1b9cc0209d6395794f654a7f0764586
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-08-07 21:45:28 +0900

    適当なクラスを実装

--- Sample/Core/Sample.cpp
+++ Sample/Core/Sample.cpp
@@ -1,1 +1,39 @@
 ﻿#include "Sample.h"
+
+namespace Core {
+
+Sample::Sample()
+    : m_message()
+{
+}
+
+
+Sample::~Sample()
+{
+}
+
+int
+Sample::countAlphabet() const
+{
+    const size_t len = this->m_message.length();
+    int cnt = 0;
+    for ( size_t i = 0; i < len; ++ i ) {
+        const char  tmp = this->m_message.at(i);
+        if ( ('A' <= tmp) && (tmp <= 'Z') ) {
+            ++ cnt;
+        }
+        if ( ('a' <= tmp) && (tmp <= 'z') ) {
+            ++ cnt;
+        }
+    }
+
+    return ( static_cast<int>(cnt) );
+}
+
+void
+Sample::setMessage(const std::string &message)
+{
+    this->m_message = message;
+}
+
+}
--- Sample/Core/Sample.h
+++ Sample/Core/Sample.h
@@ -1,4 +1,21 @@
 ﻿#pragma once
+
+#include    <string>
+
+namespace Core {
+
 class Sample
 {
+public:
+    Sample();
+    virtual ~Sample();
+
+public:
+    int countAlphabet() const;
+    void setMessage(const std::string &message);
+
+private:
+    std::string     m_message;
 };
+
+}
