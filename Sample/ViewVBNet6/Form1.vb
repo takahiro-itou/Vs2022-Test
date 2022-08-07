2585cc27ff9384d27f17cd88267f1d3ebeec6695
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-08-07 22:05:05 +0900

    フォームのイベントハンドラを実装

--- Sample/ViewVBNet6/Form1.vb
+++ Sample/ViewVBNet6/Form1.vb
@@ -1,6 +1,16 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+    Private Sub RunCount(ByVal message As String)
+        Dim result As Integer
+        Dim objWrapper As Wrapper.SampleWrapper = New Wrapper.SampleWrapper
+
+        objWrapper.setMessage(message)
+        result = objWrapper.countAlphabet()
+        MsgBox("入力した文字列中のアルファベットの個数は " & result, MsgBoxStyle.OkOnly)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RunCount(TextBox1.Text)
     End Sub
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
@@ -16,6 +26,6 @@
     End Sub
 
     Private Sub CountToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CountToolStripMenuItem.Click
-
+        RunCount(TextBox1.Text)
     End Sub
 End Class
